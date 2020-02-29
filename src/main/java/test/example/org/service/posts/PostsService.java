28b3a8dc74c1f743a3beb10bf87c2cd2aca30704
zzingyuna <kya7637@hanmail.net>
2020-02-29 19:07:18 +0900

    mustache_update,delete

--- src/main/java/test/example/org/service/posts/PostsService.java
+++ src/main/java/test/example/org/service/posts/PostsService.java
@@ -5,9 +5,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import test.example.org.domain.posts.Posts;
 import test.example.org.domain.posts.PostsRepository;
+import test.example.org.web.dto.PostsListResponseDto;
 import test.example.org.web.dto.PostsResponseDto;
 import test.example.org.web.dto.PostsSaveRequestDto;
 import test.example.org.web.dto.PostsUpdateRequestDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +37,20 @@ public class PostsService {
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(
+                ()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        postsRepository.delete(posts);
     }
 }
